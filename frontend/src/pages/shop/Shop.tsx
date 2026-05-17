1c6551f0147e26fd52003f14d256b6880db30c0e
Miryl <hntb0102@gmail.com>
2026-05-17 20:29:21 +0700

    feat(shop): add Shop listing and Product Detail pages

---
+++ frontend/src/pages/shop/Shop.tsx
@@ -0,0 +1,7 @@
+export default function Shop() {
+  return (
+    <div className="mx-auto max-w-[1920px] px-4 py-8 md:px-6 lg:px-8">
+      <h1 className="text-heading1 font-heading text-nike-black">Shop All</h1>
+    </div>
+  )
+}
